0ff1fdd2df1eb776d575d2d315921660dcdc3cc5
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2023-10-30 21:04:55 -0300

    apply some visual changes

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -70,15 +70,22 @@ class _LoginForm extends ConsumerWidget {
             label: 'Correo',
             keyboardType: TextInputType.emailAddress,
             onChanged: ref.read(loginFormProvider.notifier).onEmailChange,
-            errorMessage: loginForm.email.errorMessage,
+            errorMessage:
+              loginForm.isFormPosted
+                ? loginForm.email.errorMessage
+                : null,
           ),
+
           const SizedBox( height: 30 ),
 
           CustomTextFormField(
             label: 'Contraseña',
             obscureText: true,
             onChanged: ref.read(loginFormProvider.notifier).onPasswordChange,
-            errorMessage: loginForm.password.errorMessage,
+            errorMessage:
+              loginForm.isFormPosted
+                ? loginForm.password.errorMessage
+                : null,
           ),
     
           const SizedBox( height: 30 ),
