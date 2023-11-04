cd9c9b6cbb3b070578aca782903da2864b93430a
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2023-11-03 22:17:31 -0300

    implement token saving and deleting

--- lib/features/auth/presentation/providers/auth_provider.dart
+++ lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,18 +1,26 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_sotrage_service.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service_impl.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = AuthRepositoryImpl();
+  final keyValueStorageService = KeyValueStorageServiceImpl();
 
-  return AuthNotifier(authRepository: authRepository);
+  return AuthNotifier(
+    authRepository: authRepository,
+    keyValueStorageService: keyValueStorageService,
+  );
 });
 
 class AuthNotifier extends StateNotifier<AuthState> {
   final AuthRepository authRepository;
+  final KeyValueStorageService keyValueStorageService;
 
   AuthNotifier({
     required this.authRepository,
+    required this.keyValueStorageService,
   }) : super(AuthState());
 
   Future<void> loginUser(String email, String password) async {
@@ -31,8 +39,9 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   void checkAuthStatus(String email, String password) async {}
 
-  void _setLoggedUser(User user) {
-    // todo: save token phisically
+  void _setLoggedUser(User user) async {
+    await keyValueStorageService.setKeyValue('token', user.token);
+
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
@@ -43,6 +52,8 @@ class AuthNotifier extends StateNotifier<AuthState> {
   }
 
   Future<void> logout([String? errorMessage]) async {
+    await keyValueStorageService.removeKey('token');
+
     state.copyWith(
       authStatus: AuthStatus.notAuthenticated,
       user: null,
