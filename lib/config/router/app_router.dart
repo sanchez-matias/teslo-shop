e9fc6a62de7583b0c6229b0989a23cff13627b4c
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2023-11-04 00:45:54 -0300

    create a provider on our app router and a checking auth status screen

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,26 +1,39 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:teslo_shop/features/auth/auth.dart';
 import 'package:teslo_shop/features/products/products.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/login',
-  routes: [
+final goRouterProvider = Provider((ref) {
+  return GoRouter(
+    initialLocation: '/splash',
+    routes: [
+      //* Checking auth status screen
+      GoRoute(
+        path: '/splash',
+        builder: (context, state) => const CheckAuthStatusScreen(),
+      ),
 
-    ///* Auth Routes
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
-    GoRoute(
-      path: '/register',
-      builder: (context, state) => const RegisterScreen(),
-    ),
+      ///* Auth Routes
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/register',
+        builder: (context, state) => const RegisterScreen(),
+      ),
 
-    ///* Product Routes
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const ProductsScreen(),
-    ),
-  ],
-  ///! TODO: Bloquear si no se está autenticado de alguna manera
-);
+      ///* Product Routes
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const ProductsScreen(),
+      ),
+    ],
+
+    redirect: (context, state) {
+      // todo: complete redirection conditions
+      return null;
+    },
+  );
+  
+});
