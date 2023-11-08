ce6bd7525bd65a02bce5dad5fc916c091c7b044a
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2023-11-08 14:07:24 -0300

    lock submit button on posting

--- lib/features/auth/presentation/providers/login_form_provider.dart
+++ lib/features/auth/presentation/providers/login_form_provider.dart
@@ -86,7 +86,11 @@ class LoginFormNotifier extends StateNotifier<LoginFormState> {
     // The state must be valid if we want to continue, my bad.
     if (!state.isValid) return;
 
+    state = state.copyWith(isPosting: true);
+
     await loginUserCallback(state.email.value, state.password.value);
+
+    state = state.copyWith(isPosting: false);
   }
 
   _touchEveryField() {
