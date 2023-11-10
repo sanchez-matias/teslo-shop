f533a248912992345cd1b1b59d7406199eb2b0e2
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2023-11-10 10:41:13 -0300

    Implement infinite scroll on products screen

--- lib/features/products/presentation/screens/products_screen.dart
+++ lib/features/products/presentation/screens/products_screen.dart
@@ -43,7 +43,11 @@ class _ProductsViewState extends ConsumerState<_ProductsView> {
   @override
   void initState() {
     super.initState();
-    ref.read(productsProvider.notifier).loadNextPage();
+    scrollController.addListener(() {
+      if (scrollController.position.pixels + 400 >= scrollController.position.maxScrollExtent) {
+        ref.read(productsProvider.notifier).loadNextPage();
+      }
+    });
   }
 
   @override
@@ -60,6 +64,7 @@ class _ProductsViewState extends ConsumerState<_ProductsView> {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10),
       child: MasonryGridView.count(
+        controller: scrollController,
         physics: const BouncingScrollPhysics(),
         crossAxisCount: 2,
         mainAxisSpacing: 20,
