d669d5e21a639ce30b33458074d50c2c951e5b31
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2023-11-10 09:42:25 -0300

    Create providers for products

---
+++ lib/features/products/presentation/providers/product_repository_provider.dart
@@ -0,0 +1,18 @@
+// The objective of this provider is to set the instance of
+// ProductsRepositoryImpl along the whole app.
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:teslo_shop/features/auth/presentation/providers/auth_provider.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+import 'package:teslo_shop/features/products/infrastructure/infrastructure.dart';
+
+final productsRepositoryProvider = Provider<ProductsRepository>((ref) {
+  
+  final accessToken = ref.watch(authProvider).user?.token ?? '';
+
+  final productsRepository = ProductsRepositoryImpl(
+    ProductsDatasourceImpl(accessToken: accessToken)
+  );
+
+  return productsRepository;
+});
