21e32e69a59e0f4e3bf7c49ac231b6a8c1f4dc69
Matias Sanchez <matias.emanuel.sanchez@hotmail.com>
2023-11-14 00:19:11 -0300

    Visual changes on product screen

--- lib/features/products/presentation/screens/product_screen.dart
+++ lib/features/products/presentation/screens/product_screen.dart
@@ -17,6 +17,11 @@ class ProductScreen extends ConsumerWidget {
     required this.productId,
   });
 
+  void showSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Product Updated')));
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final productState = ref.watch(productProvider(productId));
@@ -38,8 +43,14 @@ class ProductScreen extends ConsumerWidget {
         onPressed: () {
           if (productState.product == null) return;
 
-          ref.read(productFormProvider(productState.product!).notifier
-          ).onFormSubmit();
+          ref.read(productFormProvider(productState.product!).notifier)
+            .onFormSubmit()
+            .then((value) {
+              if (value == false) return;
+              showSnackBar(context);
+            });
+
+
         },
         child: const Icon(Icons.save_rounded),
       ),
@@ -66,7 +77,12 @@ class _ProductView extends ConsumerWidget {
         ),
         const SizedBox(height: 10),
         Center(
-            child: Text(productForm.title.value, style: textStyles.titleSmall)),
+            child: Text(
+              productForm.title.value,
+              style: textStyles.titleSmall,
+              textAlign: TextAlign.center,
+            )),
+
         const SizedBox(height: 10),
         _ProductInformation(product: product),
       ],
